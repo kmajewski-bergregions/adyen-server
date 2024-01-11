3fbc83e7f3783bb04e4e01e5ea981c0dcd5c1ffd
krzysztof <majewski.kielce@gmail.com>
2024-01-11 11:22:58 +0100

    first commit

---
+++ src/main/java/com/example/adyenserver/AdyenServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.adyenserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdyenServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdyenServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/adyenserver/AdyenServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.adyenserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdyenServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
